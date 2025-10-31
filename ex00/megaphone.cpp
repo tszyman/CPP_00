b8163a8a16554c701c43321e0eed2df27701b1f0
Tomasz Szymanski <tszymans@student.42.fr>
2025-10-31 09:40:31 +0100

    A bit more C++ like syntax

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+
+class UpperCase
+{
+private:
+	std::string value;
+
+public:
+	UpperCase(const std::string &str) : value(str) {}
+
+	std::string to_upper_case() const
+{
+		std::string result = value;
+		for (size_t i = 0; i < result.length(); ++i)
+		{
+			if (std::islower(result[i]))
+				result[i] = std::toupper(result[i]);
+}
+		return result;
+	}
+};
 
 int main(int argc, char **argv)
 {
-	int	i;
-	int	j;
+	int i;
 
-	if (argc > 1)
+	if (argc < 2)
 	{
-		i = 1;
-		while (i < argc)
-		{
-			j = 0;
-			while (argv[i][j])
-			{
-				std::cout << static_cast<char>(std::toupper(argv[i][j]));
-				j++;
-			}
-			i++;
-		}
-		std::cout << std::endl;
-		return 0;
+		std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 1;
 	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	i = 1;
+	while (i < argc)
+	{
+		UpperCase word(argv[i]);
+		std::cout << word.to_upper_case();
+		++i;
+	}
+	std::cout << std::endl;
 	return 0;
 }
