f962652c0c784f01f7a0efa73d65981c2ba1d934
Tomasz Szymanski <tszymans@student.42.fr>
2025-11-03 20:43:29 +0100

    [ex01] phonebook working

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,11 +6,27 @@ PhoneBook::~PhoneBook(void) {}
 
 void PhoneBook::addContact(const Contact& contact)
 {
-
+	if (_count < 8)
+	{
+		_contacts[_count] = contact;
+		_count++;
+	}
+	else
+	{
+		_contacts[_oldestIndex] = contact;
+		_oldestIndex = (_oldestIndex + 1) % 8;
+	}
 }
 const Contact* PhoneBook::getContact(int index) const
 {
-
+	if (index < 0 || index >= _count)
+	{
+		return 0;
+	}
+	else
+	{
+		return &_contacts[index];
+	}
 }
 
 int PhoneBook::getStoredContactsCount(void) const
@@ -20,15 +36,45 @@ int PhoneBook::getStoredContactsCount(void) const
 
 void PhoneBook::displayContactsSummary(void) const
 {
+	int	i;
 
+	std::cout << std::right << std::setw(10) << "Index" << '|'
+			<< std::right << std::setw(10) << "First Name" << '|'
+			<< std::right << std::setw(10) << "Last Name" << '|'
+			<< std::right << std::setw(10) << "Nickname" << '\n';
+	i = 0;
+	while (i < _count)
+	{
+		std::cout << std::right << std::setw(10) << i << '|'
+				<< std::right << std::setw(10) << _formatField(_contacts[i].getFirstName()) << '|'
+				<< std::right << std::setw(10) << _formatField(_contacts[i].getLastName()) << '|'
+				<< std::right << std::setw(10) << _formatField(_contacts[i].getNickName()) << '\n';
+		i++;
+	}
 }
 
 void PhoneBook::displayContactDetails(int index) const
 {
-
+	if (index < 0 || index >= _count)
+	{
+		std::cout << "Invalid index\n";
+		return;
+	}
+	else
+	{
+		const Contact& c = _contacts[index];
+		std::cout << "First Name: " << c.getFirstName() << '\n'
+				<< "Last Name: " << c.getLastName() << '\n'
+				<< "Nickname: " << c.getNickName() << '\n'
+				<< "Phone number: " << c.getPhoneNumber() << '\n'
+				<< "Darkest secret: " << c.getDarkestSecret() << '\n';
+	}
 }
 
 std::string PhoneBook::_formatField(const std::string& str)
 {
-
+	if (str.length() > 10)
+		return str.substr(0,7) + "...";
+	else
+		return str;
 }
---
+++ ex01/main.cpp
@@ -0,0 +1,95 @@
+#include "PhoneBook.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+static int	stringToInt(const std::string &str)
+{
+	std::istringstream iss(str);
+	int num;
+
+	num = 0;
+	iss >> num;
+	return num;
+}
+
+static bool getContactInput(Contact &contact)
+{
+	std::string input;
+
+	std::cout << "Enter First Name: ";
+	std::getline(std::cin, input);
+	if (!contact.setFirstName(input))
+		return false;
+	std::cout << "Enter Last Name: ";
+	std::getline(std::cin, input);
+	if (!contact.setLastName(input))
+		return false;
+	std::cout << "Enter Nickname: ";
+	std::getline(std::cin, input);
+	if (!contact.setNickName(input))
+		return false;
+	std::cout << "Enter Phone Number: ";
+	std::getline(std::cin, input);
+	if (!contact.setPhoneNumber(input))
+		return false;
+	std::cout << "Enter Darkest Secret: ";
+	std::getline(std::cin, input);
+	if (!contact.setDarkestSecret(input))
+		return false;
+	return contact.isValid();
+}
+
+int	main(void)
+{
+	PhoneBook	phoneBook;
+	std::string	command;
+
+	std::cout << "I'm you're PhoneBook!\n";
+	while (true)
+	{
+		std::cout << "Enter a command (only ADD, SEARCH, EXIT allowed): ";
+		std::getline(std::cin, command);
+		if (command == "EXIT")
+		{
+			break;
+		}
+		else if (command == "ADD")
+		{
+			Contact contact;
+			if (getContactInput(contact))
+			{
+				phoneBook.addContact(contact);
+				std::cout << "New contact added!\n";
+			}
+			else
+			{
+				std::cout << "All fields are required! Adding contact failed!\n";
+			}
+		}
+		else if (command == "SEARCH")
+		{
+			if (phoneBook.getStoredContactsCount() == 0)
+			{
+				std::cout << "No contacts in the phonebook yet!\n";
+				continue;
+			}
+			phoneBook.displayContactsSummary();
+			std::cout << "Enter contact index to view the details: ";
+			std::getline(std::cin, command);
+			int indx = -1;
+			indx = stringToInt(command);
+			if (indx >= 0 && indx < phoneBook.getStoredContactsCount())
+			{
+				phoneBook.displayContactDetails(indx);
+			}
+			else
+			{
+				std::cout << "No such index!\n";
+				continue;
+			}
+		}
+	}
+	std::cout << "Thank you! See you next time!\n";
+	return (0);
+}
